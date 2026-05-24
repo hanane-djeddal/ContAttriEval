af981184b1f889d6d0ebd802376085697055d74e
Hanane Djeddal <48019914+hanane-djeddal@users.noreply.github.com>
2026-05-24 14:07:38 +0200

    Modify learning rate and remove unused inference code
    Updated learning rate and cleaned up commented code.

--- model_training/run_qwen_contrast.sh
+++ model_training/run_qwen_contrast.sh
@@ -10,9 +10,8 @@ export TRANSFORMERS_OFFLINE=1
 for model in "${models[@]}"; do
     # 32 1e-5
     # ***************** Set parameters here *****************
-    dataset_version=attributionBench_hardpos_augmentedQwen30B_allerrors_contrastive #attributionBench_augmentedQwen30B_allerrors_contrastive_extendalltrain #attributionBench_augmentedQwen30B_allerrors_contrastive_extendalltrainshuffled #   
-    #template=base_c_e
-    lr=5e-5 #3e-5  #1e-5 #5e-5 #
+    dataset_version=attributionBench_hardpos_augmentedQwen30B_allerrors_contrastive 
+    lr=1e-5 #5e-5 #
     cont_weight=1.0 #0.5 #0.1  #1e-5  0.5 #
     classif_weight=1.0
     num_train_epoches=5
@@ -86,18 +85,4 @@ for model in "${models[@]}"; do
         --max_length 2048  \
         --max_new_tokens 6 \
         --template ${template}
-
-    # zero-shot
-    # python ../src/inference/run_inference.py \
-    #     --method autoais \
-    #     --data_path AttributionBench \
-    #     --dataset_version ${dataset_version} \
-    #     --template_path ../src/prompts.json \
-    #     --model_name $model \
-    #     --bs 4 \
-    #     --split test \
-    #     --output_dir ../inference_results/${dataset_version} \
-    #     --max_length 2048  \
-    #     --max_new_tokens 6 \
-    #     --template ${template}
 done
